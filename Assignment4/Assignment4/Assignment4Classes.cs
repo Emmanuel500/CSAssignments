5ebb4a1de7bc203a0238988c2c00d66bdd4d39e1
Emmanuel500 <32080181+Emmanuel500@users.noreply.github.com>
2022-02-12 10:48:47 -0500

    Assignment 4 Started

---
+++ Assignment4/Assignment4/Assignment4Classes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+//Practice working with Generics
+
+//1) Stack class
+public class MyStack<T>
+{
+	//Field
+	private List<T> stackList = new List<T>();
+	private int position = 0;
+
+	//Methods
+	//Counting the stack
+	public int Count()
+    {
+		return stackList.Count;
+    }
+	//Pop stack
+	public T Pop()
+    {
+		T item = stackList[position];
+		stackList.RemoveAt(position);
+		return item;
+    }
+	//Push stack
+	public void Push()
+    {
+
+    }
+}
+
+//2) 
+public class MyList<T>
+{
+	//Field
+
+	//Method
+
+}
+
+//3)
+public interface IRepository<T>
+{
+	void Add(T item);
+	void Remove(T item);
+	void Save();
+	IEnumerable<T> GetAll();
+	T GetById(int id);
+}
+
+public class GenericRepository<T>: IRepository<T>
+{
+	//Field
+
+	//Methods
+
+}
---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,41 @@
+﻿//Assignment 4
+
+//Test Your Knowledge
+//1.Describe the problem generics address.
+//The dry principle to not repeat, losing type-safety, and unwanted boxing problems get address by generics
+
+//2.How would you create a list of strings, using the generic List class?
+//List<string> genericStringList = new List<string>();
+
+//3.How many generic type parameters does the Dictionary class have?
+//It has two generic type parameters
+//Dictionary<int, string> dictionaryTest = new Dictionary<int, string>();
+
+//4.True/False. When a generic class has multiple type parameters, they must all match.
+//False
+
+//5.What method is used to add items to a List object?
+//Add method is used to add items to a list
+
+//6.Name two methods that cause items to be removed from a List.
+//Clear method removes all items
+//Remove method removes the first item to be added or a specific item
+
+//7.How do you indicate that a class has a generic type parameter?
+// By the <T> that appears after the class name
+
+//8.True/False. Generic classes can only have one generic type parameter.
+//False
+
+//9.True/False. Generic type constraints limit what can be used for the generic type.
+//True
+
+//10.True/False. Constraints let you use the methods of the thing you are constraining to.
+//True
+
+//Testing Classes
+//1) Stack
+
+//2) List
+
+//3) Generic Repository
